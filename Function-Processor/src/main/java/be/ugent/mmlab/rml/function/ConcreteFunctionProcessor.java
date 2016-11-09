e0adc6184e1f670cb328904fba944b6adf43210e
wmaroy <maroy.wouter@gmail.com>
2016-11-09 16:03:27 +0100

    DBpedia functions added

--- Function-Processor/src/main/java/be/ugent/mmlab/rml/function/ConcreteFunctionProcessor.java
+++ Function-Processor/src/main/java/be/ugent/mmlab/rml/function/ConcreteFunctionProcessor.java
@@ -1,6 +1,7 @@
 package be.ugent.mmlab.rml.function;
 
 
+import java.io.File;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -16,7 +17,7 @@ public class ConcreteFunctionProcessor {
     private FunctionHandler handler;
 
     public ConcreteFunctionProcessor() {
-        this.handler = new FunctionHandler("../resources/functions");
+        this.handler = new FunctionHandler(new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()).getParent() + "/resources/functions");
     }
 
     // Log
@@ -27,7 +28,10 @@ public class ConcreteFunctionProcessor {
             String function, Map<String, String> parameters) {
         FunctionModel fn = this.handler.get(function);
         if (fn == null) {
+            System.err.println("The function " + function + " was not defined.");
             log.error("The function " + function + " was not defined.");
+            //TODO: wmaroy:
+            return "undefined";
         }
         return fn.execute(parameters).toString();
     }
