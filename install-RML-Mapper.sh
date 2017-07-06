8abfca59aad4719eb726f1efab483fb034559aa4
wmaroy <maroy.wouter@gmail.com>
2017-07-06 15:05:43 +0200

    added install script for installing RML-Mapper in local mvn repo

---
+++ install-RML-Mapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script installs the RML-Mapper in this folder into the local mvn repos
+#cp RML-Mapper.jar /root/.m2/repository/be/ugent/mmlab/rml/RML-Mapper/1.1/RML-Mapper-1.1.jar
+cp RML-Mapper.jar /home/wmaroy/.m2/repository/be/ugent/mmlab/rml/RML-Mapper/1.1/RML-Mapper-1.1.jar
